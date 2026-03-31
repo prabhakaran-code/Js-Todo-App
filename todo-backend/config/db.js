700dfbef23377b3de6261bc3aa4e8b276ccee5c8
prabhakaran-code <prabhakaran.u@hubino.com>
2026-03-31 18:02:36 +0530

    backend-deployment

--- todo-backend/config/db.js
+++ todo-backend/config/db.js
@@ -1,16 +1,20 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "root",
- database: "todo_app_js"
+const db = mysql.createPool({
+ host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect(err=>{
- if(err) throw err;
- console.log("MySQL Connected");
+if (err) {
+    console.error(" DB Connection Error:", err.message);
+    return;
+  }
+  console.log(" MySQL Connected");
 });
 
 module.exports = db;
